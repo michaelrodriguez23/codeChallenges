50deda81c9cfbabcf1dabac175b44004348daaf9
Michael Rodriguez <michaelrodriguez23@gmail.com>
2020-11-17 11:02:45 -0500

    Sock pairs

--- SockPairs/src/main/java/com/mr/sockpairs/Solution.java
+++ SockPairs/src/main/java/com/mr/sockpairs/Solution.java
@@ -1,8 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Alex works at a clothing store. There is a large pile of socks that must be paired by color for sale.
+Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+For example, there are  socks with colors .
+There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+Function Description
+
+Complete the sockMerchant function in the editor below. 
+It must return an integer representing the number of matching pairs of socks 
+that are available.
+
+sockMerchant has the following parameter(s):
+
+n: the number of socks in the pile
+ar: the colors of each sock
+Input Format
+
+The first line contains an integer , the number of socks represented in .
+The second line contains  space-separated integers describing the colors  of the socks in the pile.
+https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup 
+*/
 package com.mr.sockpairs;
 
 import java.util.Arrays;
